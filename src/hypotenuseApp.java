2b3a32354fd4d75582ef155f33915dec7d47edca
Faye Carpenter <carpenp@email.uscb.edu>
2026-02-15 03:21:23 -0500

    Began Psuedocode.

--- src/hypotenuseApp.java
+++ src/hypotenuseApp.java
@@ -3,5 +3,13 @@
  * @author patrickcarpenter
  */
 public class hypotenuseApp {
+    // This is the one I'm most nervous about. The word "Hypotenuse" in the 
+    // context of Java is horrifying.
+    
+    /*Init. Phase*/
+    
+    /*Processing Phase*/
+    
+    /*Termination Phase*/
     
 }
