93ba246557a3b94c7d0b7e74d542040d83992873
Faye Carpenter <carpenp@email.uscb.edu>
2026-02-16 17:43:59 -0500

    Programing project finished.

--- src/hypotenuseApp.java
+++ src/hypotenuseApp.java
@@ -1,26 +1,45 @@
+import java.util.Scanner;
+
 /**
- *
- * @author patrickcarpenter
+ * Should have added this sooner:
+ * Credit to GeeksForGeeks for help with code!
+ * @author Faye Carpenter
  */
 public class hypotenuseApp {
     // This is the one I'm most nervous about. The word "Hypotenuse" in the 
     // context of Java is horrifying.
     
     /*Init. Phase*/
-    // Import Math Class
-    // Initalize Double Arg1
-    // Initalize Double Arg2
-    // Initalize Double Hypotenuse
+    // double arg1;
+    // double arg2;
+    // double hypotenuse;
+    public static void main(String[] args){
+        /*Processing Phase*/
+        Scanner sc = new Scanner(System.in);
+        // Solicit user input for Arg1
+        System.out.print("Insert side A of the Triangle: ");
+        double arg1 = sc.nextDouble();
+        sc.nextLine();
         
-    /*Processing Phase*/
-    // Solicit user input for Arg1
-    // Solicit user input for Arg2
-    // Call Hypotenuse Method using Arg1 and Arg2
+        // Solicit user input for Arg2
+        System.out.print("Insert side B of the Triangle: ");
+        double arg2 = sc.nextDouble();
+        sc.nextLine();
+        
+        // Call Hypotenuse Method using Arg1 and Arg2
+        double hypo = hypotenuse(arg1, arg2);
+        
+        /*Termination Phase*/
+        // Print Arg1, Arg2, and Hypotenuse
+        System.out.print("A traigle with a side A equaling " + arg1 + " and a side B equaling " + arg2 );
+        System.out.print(" has a Hypotenuse of " + hypo + ".\n");
+     } // end main
+   
+    /* Hypotenuse Method */
+    public static double hypotenuse(double x, double y) {
     
-    // Hypotenuse Method
-        // Return Hypotenuse = Math.sqrt(Math.pow(Arg1, 2) + Math.pow(Arg2, 2))
+        double z =  Math.hypot(x, y); // Math.sqrt((Math.pow(x, 2) + Math.pow(y, 2))); can also be used
+        return z;
     
-    
-    /*Termination Phase*/
-    // Print Arg1, Arg2, and Hypotenuse
-}
+    } // end hypotenuse method
+} // end class 
